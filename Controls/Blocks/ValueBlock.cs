39d3d410b98064cca1eb942941e87f289650deb5
script-1024 <benny20050218@gmail.com>
2024-03-10 21:45:33 +0800

    [+] 新的方块种类

---
+++ Controls/Blocks/ValueBlock.cs
@@ -0,0 +1,33 @@
+﻿using Windows.UI;
+using CodeBlocks.Core;
+
+namespace CodeBlocks.Controls
+{
+    public enum BlockValueType { None = 0, Text = 1, Number = 2, Boolean = 3 }
+
+    public class ValueBlock : BaseBlock
+    {
+        private BlockValueType type;
+
+        public BlockValueType ValueType
+        {
+            get => type;
+            set
+            {
+                type = value;
+                OnTypeChanged();
+            }
+        }
+
+        public ValueBlock() : base()
+        {
+            BlockColor = Color.FromArgb(255, 150, 25, 25);
+            MetaData = new() { Type = BlockType.ValueBlock, Variant = 1, Size = this.Size };
+        }
+
+        private void OnTypeChanged()
+        {
+            
+        }
+    }
+}
