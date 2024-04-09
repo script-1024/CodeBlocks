ec0585abf33bc7e0be776f90f2ec2749eecce02e
script-1024 <benny20050218@gmail.com>
2024-04-09 20:32:45 +0800

    方便好用的讯息对话框

--- Libraries/Utils.cs
+++ Libraries/Utils.cs
@@ -1,8 +1,70 @@
 ﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.Threading.Tasks;
+using System;
 using Windows.UI;
 
 namespace CodeBlocks.Core
 {
+    public enum DialogVariant
+    {
+        Yes = 1, No = 2, Cancel = 4, Confirm = 8, Save = 16, Giveup = 32,
+        YesNo = 3, YesCancel = 5, YesNoCancel = 7, ConfirmCancel = 12, SaveGiveupCancel = 52,
+    }
+
+    public class MessageDialog
+    {
+        private readonly ContentDialog dialog = new();
+        public XamlRoot XamlRoot { get => dialog.XamlRoot; set => dialog.XamlRoot = value; }
+        private string GetLocalizedString(string key) => (Application.Current as App).Localizer.GetString(key);
+        public async Task<ContentDialogResult> ShowAsync(string msgId, DialogVariant variant = DialogVariant.Confirm)
+        {
+            bool hasPrimaryButton = false;
+            dialog.Title = GetLocalizedString($"Messages.{msgId}.Title");
+            dialog.Content = GetLocalizedString($"Messages.{msgId}.Description");
+            dialog.PrimaryButtonText = dialog.SecondaryButtonText = dialog.CloseButtonText = null; // 重置按键文本
+
+            if ((int)(variant & DialogVariant.Yes) > 0)
+            {
+                dialog.PrimaryButtonText = GetLocalizedString("Messages.Button.Yes");
+                hasPrimaryButton = true;
+            }
+            if ((int)(variant & DialogVariant.Confirm) > 0)
+            {
+                
+                dialog.PrimaryButtonText = GetLocalizedString("Messages.Button.Confirm");
+                hasPrimaryButton = true;
+            }
+            if ((int)(variant & DialogVariant.Save) > 0)
+            {
+                dialog.PrimaryButtonText = GetLocalizedString("Messages.Button.Save");
+                hasPrimaryButton = true;
+            }
+            if ((int)(variant & DialogVariant.Giveup) > 0)
+            {
+                dialog.SecondaryButtonText = GetLocalizedString("Messages.Button.Giveup");
+            }
+            if ((int)(variant & DialogVariant.Cancel) > 0)
+            {
+                dialog.CloseButtonText = GetLocalizedString("Messages.Button.Cancel");
+            }
+
+            if (variant == DialogVariant.No || variant == DialogVariant.YesNo)
+            {
+                dialog.CloseButtonText = GetLocalizedString("Messages.Button.No");
+            }
+            else if (variant == DialogVariant.YesNoCancel)
+            {
+                dialog.SecondaryButtonText = GetLocalizedString("Messages.Button.No");
+            }
+
+            if (hasPrimaryButton) dialog.DefaultButton = ContentDialogButton.Primary;
+            else dialog.DefaultButton = ContentDialogButton.Close;
+
+            return await dialog.ShowAsync();
+        }
+    }
+
     public static class Utils
     {
         public static Visibility ToVisibility(bool value) => (value) ? Visibility.Visible : Visibility.Collapsed;
