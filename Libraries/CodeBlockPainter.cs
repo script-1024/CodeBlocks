0acea18153284d1e064425bcbd006584aa5ac297
script-1024 <benny20050218@gmail.com>
2024-06-13 13:39:48 +0800

    支持绘制流程块

--- Libraries/CodeBlockPainter.cs
+++ Libraries/CodeBlockPainter.cs
@@ -1,33 +1,40 @@
-﻿using Windows.Foundation;
+﻿using System;
+using Windows.Foundation;
 using CodeBlocks.Controls;
 using Microsoft.UI.Xaml.Media;
 
 namespace CodeBlocks.Core
 {
-    public enum BlockType
+    public enum BlockType { Undefined = 0, Event = 1, Process = 2, Action = 3, Value = 4 }
+
+    public struct BlockBranchData
     {
-        Undefined = 0, Event = 1, Control = 2, Action = 3, Value = 4
+        public int Slots;
+        public double BarHeight, InnerHeight;
     }
 
     public struct BlockMetaData
     {
         public BlockType Type;
-        public byte Variant;
         public string Code;
+        public byte Variant;
         public int Slots;
         public Size Size;
-        public static readonly BlockMetaData Null = new() { Type = 0, Code = "", Variant = 0, Slots = 0, Size = default };
+        public BlockBranchData[] Parts;
+        public static readonly BlockMetaData Null = new()
+        {
+            Code = "", Type = 0, Variant = 0, Slots = 0,
+            Size = default, Parts = []
+        };
     }
 
     public class CodeBlockPainter
     {
-        public double Width { get; set; }
-        public double Height { get; set; }
-
         public bool IsExpand;
         public BlockMetaData MetaData;
 
         private double x, y;
+        private double blockWidth, blockHeight;
         private readonly int w = CodeBlock.SlotWidth;
         private readonly int h = CodeBlock.SlotHeight;
         private PathFigure pathFigure;
@@ -44,7 +51,7 @@ namespace CodeBlocks.Core
             LineSegment line3 = new() { Point = new Point(x, y -= h) };
             pathFigure.Segments.Add(line3);
         }
-
+        // 0 1
         private void DrawLeftOrRightCurve(int sign)
         {
             // Sign : Left -1 | Right 1
@@ -58,30 +65,33 @@ namespace CodeBlocks.Core
             pathFigure.Segments.Add(line3);
         }
 
-        private void DrawLine(double dx = 0, double dy = 0, double? newX = null, double? newY = null, bool isRelative = true)
+        /// <summary>
+        /// 绘制直线
+        /// </summary>
+        /// <param name="dx">水平变化量</param>
+        /// <param name="dy">垂直变化量</param>
+        /// <param name="newX">赋给 x 的新值</param>
+        /// <param name="newY">赋给 y 的新值</param>
+        private void DrawLine(double? newX = null, double? newY = null, double dx = 0, double dy = 0)
         {
             LineSegment line = new();
+            x = (newX != null) ? (double)newX : x;
+            y = (newY != null) ? (double)newY : y;
 
-            if (isRelative)
-            { 
-                x += dx;
-                y += dy;
-            }
-            else
-            {
-                if (newX != null) x = (double)newX;
-                if (newY != null) y = (double)newY;
-            }
-
-            line.Point = new Point(x, y);
+            line.Point = new Point(x += dx, y += dy);
             pathFigure.Segments.Add(line);
         }
 
         public PathGeometry DrawBlockBorder()
         {
-            Width = MetaData.Size.Width;
-            Height = MetaData.Size.Height;
+            blockWidth = MetaData.Size.Width;
+            blockHeight = MetaData.Size.Height;
             int slots = MetaData.Slots;
+
+            // Only used for process blocks, always be zero for otherwise
+            // 分支数量，用于循环(for/while)、判断(if-then-else)、切换(switch-case-default)、尝试(try-catch-finally)等类型的方块
+            int branchCount = (MetaData.Type == BlockType.Process) ? MetaData.Variant >> 4 : 0;
+
             bool hasLeft = MetaData.Variant.HasFlag(0b_0001);
             bool hasTop = MetaData.Variant.HasFlag(0b_0010);
             bool hasRight = MetaData.Variant.HasFlag(0b_0100);
@@ -92,7 +102,18 @@ namespace CodeBlocks.Core
             pathFigure = new() { StartPoint = new Point(x = 0, y = 0) };
 
             // 上边
-            if (MetaData.Type == BlockType.Action)
+            if (MetaData.Type == BlockType.Event)
+            {
+                // 事件块的外观要特别处理
+                ArcSegment arc = new()
+                {
+                    Size = new Size(35, 24),
+                    Point = new Point(x += 60, y = 0),
+                    SweepDirection = SweepDirection.Clockwise
+                };
+                pathFigure.Segments.Add(arc);
+            }
+            else
             {
                 if (hasTop)
                 {
@@ -100,60 +121,86 @@ namespace CodeBlocks.Core
                     DrawTopOrDownCurve(1); // 凹口
                 }
             }
-            else if (MetaData.Type == BlockType.Event)
-            {
-                ArcSegment arc = new() {
-                    Size = new Size(35, 24),
-                    Point = new Point(x += 60, y = 0),
-                    SweepDirection = SweepDirection.Clockwise
-                };
-                pathFigure.Segments.Add(arc);
-            }
-            DrawLine(newX: Width - h, isRelative: false); // 其余部分
+            DrawLine(newX: blockWidth - h); // 其余部分
 
             // 右边
-            if (hasRight)
+            if (MetaData.Type == BlockType.Process)
             {
-                if (IsExpand)
+                // 流程块的外观要特别处理
+                for (int i=0; i<=branchCount; i++)
                 {
-                    DrawLine(dy: w); // 上半部分
-                    while (slots >= 1)
+                    if (!MetaData.Parts.TryGetValue(i, out var part))
+                        part = new() { Slots = 0, BarHeight = w * 3, InnerHeight = w * 3 + h };
+
+                    var barH = part.BarHeight;
+                    var innerH = part.InnerHeight;
+                    var pSlots = part.Slots;
+
+                    if (IsExpand && pSlots > 0)
                     {
-                        DrawLeftOrRightCurve(1); // 凹口
-                        if (--slots >= 1) DrawLine(0, w * 2);
+                        while (pSlots > 0)
+                        {
+                            DrawLine(dy: w); // 上半部分
+                            DrawLeftOrRightCurve(1); // 凹口
+                            DrawLine(dy: w);
+                            pSlots--;
+                        }
                     }
+                    else DrawLine(dy: barH);
+
+                    // 内部上边
+                    DrawLine(newX: w * 5);  // 右半部分
+                    DrawTopOrDownCurve(-1); // 凸起
+                    DrawLine(newX: w * 3);  // 其余部分
+
+                    // 内部左边
+                    DrawLine(dy: innerH); // 下半部分
+
+                    // 内部下边
+                    DrawLine(newX: blockWidth - h);
                 }
+
+                // 右侧下方其余部分
+                DrawLine(dy: w);
             }
             else
             {
-                // 后续版本才会加入方块省略状态
-                // 此时不显示右侧方块，且不允许添加/删除子方块，右侧无法互动
+                if (hasRight)
+                {
+                    if (IsExpand && slots > 0)
+                    {
+                        while (slots > 0)
+                        {
+                            DrawLine(dy: w); // 上半部分
+                            DrawLeftOrRightCurve(1); // 凹口
+                            DrawLine(dy: w);
+                            slots--;
+                        }
+                    }
+                    else DrawLine(newY: blockHeight); // 其余部分
+                }
+                else
+                {
+                    // 后续版本才会加入方块省略状态
+                    // 此时不显示右侧方块，且不允许添加/删除子方块，右侧无法互动
+                }
             }
-            DrawLine(newY: Height - h, isRelative: false); // 其余部分
 
             // 下边
-            if (hasBottom && MetaData.Type != BlockType.Value)
+            if (hasBottom)
             {
-                DrawLine(newX: w * 2, isRelative: false); // 右半部分
+                DrawLine(newX: w * 2); // 右半部分
                 DrawTopOrDownCurve(-1); // 凸起
             }
-            DrawLine(newX: 0, isRelative: false); // 其余部分
+            DrawLine(newX: 0); // 其余部分
 
             // 左边
             if (hasLeft)
             {
-                DrawLine(newY: w * 2, isRelative: false); // 下半部分
+                DrawLine(newY: w * 2); // 下半部分
                 DrawLeftOrRightCurve(-1); // 凸起
             }
-
-            if (MetaData.Type == BlockType.Value || MetaData.Type == BlockType.Action)
-            {
-                DrawLine(newY: 0, isRelative: false); // 其余部分
-            }
-            else if (MetaData.Type == BlockType.Event)
-            {
-                DrawLine(newY: 0, isRelative: false); // 其余部分
-            }
+            DrawLine(newY: 0); // 其余部分
 
             pathGeo.Figures.Add(pathFigure);
             return pathGeo;
